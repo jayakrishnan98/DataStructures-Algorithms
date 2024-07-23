0bbcd22e7cc95551ad78bc31abe2996df810eca7
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2024-07-23 23:51:23 +0530

    Time: 46 ms (98.05%), Space: 51.2 MB (32.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,10 +1,13 @@
 function twoSum(nums, target) {
+    // define a map object
     let obj = new Map();
     for (let i = 0; i < nums.length; i++) {
+        // storing the remaining value after subtracting from target value
         let rem = target - nums[i];
         if (obj.has(rem)) {
             return [obj.get(rem), i]
         }
+        // saving in the map obj for further checking
         obj.set(nums[i], i);
     }
 }
