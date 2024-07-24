5061a3d65fcd5fbf3632cc3029eaba425f64ff2f
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2024-07-25 00:10:50 +0530

    Time: 117 ms (28.63%), Space: 56.4 MB (22.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -3,15 +3,21 @@ var addTwoNumbers = function(l1, l2) {
     let current = dummyHead;
     let carry = 0;
 
-    while (l1 || l2 || carry) {
-        let x = l1 ? l1.val : 0;
-        let y = l2 ? l2.val : 0;
-        let sum = x + y + carry;
+    while (l1 !== null || l2 !== null) {
+        let x = (l1 !== null) ? l1.val : 0;
+        let y = (l2 !== null) ? l2.val : 0;
+        let sum = carry + x + y;
+
         carry = Math.floor(sum / 10);
         current.next = new ListNode(sum % 10);
         current = current.next;
-        l1 = l1 ? l1.next : null;
-        l2 = l2 ? l2.next : null;
+
+        if (l1 !== null) l1 = l1.next;
+        if (l2 !== null) l2 = l2.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
     }
 
     return dummyHead.next;
